5d28b005f6dbf0df8b5e213e8527028bb3ee57a5
Michael Porter <mdporter83@gmail.com>
2016-11-18 18:35:14 +0000

    fixed guard clause error from rubocop

--- calc.rb
+++ calc.rb
@@ -28,31 +28,22 @@ name = gets.chomp.capitalize!
 prompt("Hi, #{name}!")
 
 loop do # main loop
-
   first_num = ''
   loop do
     prompt('What is the first number?')
     first_num = gets.chomp
-
-    if valid?(first_num)
-      break
-    else
-      prompt('That doesn\'t look like a valid number')
-    end
+    break if  valid?(first_num)
+    prompt('That doesn\'t look like a valid number')
   end
 
   second_num = ''
   loop do
     prompt('What is the second number?')
     second_num = gets.chomp
-
-    if valid?(second_num)
-      break
-    else
-      prompt('That doesn\'t look like a valid number')
-    end
+    break if valid?(second_num)
+    prompt('That doesn\'t look like a valid number')
   end
- 
+
   operator_prompt = <<-MSG
     Please choose one of these valid operations...
     Add
@@ -62,31 +53,27 @@ loop do # main loop
   MSG
 
   prompt(operator_prompt)
- 
+
   operator = ''
   loop do
     operator = gets.chomp.downcase
-
-    if %w(add subtract multiply divide).include?(operator)
-      break
-    else
-      prompt('Error! Please choose a valid operation.')
-    end
+    break if %w(add subtract multiply divide).include?(operator)
+    prompt('Error! Please choose a valid operation.')
   end
 
   prompt("#{operator_to_message(operator)} the two numbers...")
-   
-   result = case operator
-            when 'add'
-              first_num.to_i + second_num.to_i
-            when 'subtract'
-              first_num.to_i - second_num.to_i
-            when 'multiply'
-              first_num.to_i * second_num.to_i
-            when 'divide'
-              first_num.to_f / second_num.to_f
-            end
-  
+
+  result = case operator
+           when 'add'
+             first_num.to_i + second_num.to_i
+           when 'subtract'
+             first_num.to_i - second_num.to_i
+           when 'multiply'
+             first_num.to_i * second_num.to_i
+           when 'divide'
+             first_num.to_f / second_num.to_f
+           end
+
   prompt("The result is #{result}.")
 
   prompt('Perform another calculation? (Type Y to perform another calculation)')
