92310b0df763145914fd02d7b14f843d404658bf
Michael Porter <mdporter83@gmail.com>
2016-11-19 22:46:23 +0000

    refactored  game.rb

--- game.rb
+++ game.rb
@@ -3,8 +3,11 @@
 VALID_CHOICES =
   %w(rock paper scissors lizard spock).freeze
 
-puts 'Welcome to Rock Paper Scissors lizard Spock'
+puts 'Welcome to Rock Paper Scissors Lizard Spock'
 puts '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+
+player_score = 0
+computer_score = 0
 
 loop do
   player = ''
@@ -25,21 +28,31 @@ loop do
     puts 'It\'s a Draw.'
   elsif player == 'rock' && (computer == 'scissors' || computer == 'lizard')
     puts 'You Win.'
+    player_score += 1
   elsif player == 'paper' && (computer == 'rock' || computer == 'spock')
     puts 'You Win.'
+    player_score += 1
   elsif player == 'scissors' && (computer == 'paper' || computer == 'lizard')
     puts 'You Win.'
+    player_score += 1
   elsif player == 'lizard' && (computer ==
     'paper' || computer == 'spock')
     puts 'You Win.'
+    player_score += 1
   elsif player == 'spock' && (computer ==
     'scissors' || computer == 'rock')
     puts 'You Win.'
+    player_score += 1
   else
     puts 'You Lose.'
+    computer_score += 1
   end
+
+  puts "You have #{player_score} points"
+  puts "The Computer has #{computer_score} points."
 
   puts 'Would you like to play again? (Y/N)'
   answer = gets.chomp.downcase
   break unless answer.start_with?('y')
 end
+
