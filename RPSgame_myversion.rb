f450b1a22ecead28e4dee53928b235677166706d
X-Michael-X <mdporter83@gmail.com>
2016-11-20 08:45:12 -0600

    Update RPSgame_myversion.rb

--- RPSgame_myversion.rb
+++ RPSgame_myversion.rb
@@ -1,10 +1,15 @@
 # Rock Paper Scissors Lizard Spock Game
 
+# I wrote the initial pre Lizard Spock version of this before
+# doing the code along version , and decided to attempt to
+# fully modify it.
+
 VALID_CHOICES =
   %w(rock paper scissors lizard spock).freeze
 
 puts 'Welcome to Rock Paper Scissors Lizard Spock'
 puts '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+puts 'First to Five Points Wins!'
 
 player_score = 0
 computer_score = 0
@@ -27,31 +32,39 @@ loop do
   if player == computer
     puts 'It\'s a Draw.'
   elsif player == 'rock' && (computer == 'scissors' || computer == 'lizard')
-    puts 'You Win.'
+    puts 'You Win This Round.'
     player_score += 1
   elsif player == 'paper' && (computer == 'rock' || computer == 'spock')
-    puts 'You Win.'
+    puts 'You Win This Round.'
     player_score += 1
   elsif player == 'scissors' && (computer == 'paper' || computer == 'lizard')
-    puts 'You Win.'
+    puts 'You Win This Round.'
     player_score += 1
   elsif player == 'lizard' && (computer ==
     'paper' || computer == 'spock')
-    puts 'You Win.'
+    puts 'You Win This Round.'
     player_score += 1
   elsif player == 'spock' && (computer ==
     'scissors' || computer == 'rock')
-    puts 'You Win.'
+    puts 'You Win This Round.'
     player_score += 1
   else
-    puts 'You Lose.'
+    puts 'You Lose This Round.'
     computer_score += 1
   end
 
   puts "You have #{player_score} points"
   puts "The Computer has #{computer_score} points."
 
-  puts 'Would you like to play again? (Y/N)'
+  if player_score == 5
+    puts 'You reached Five Points, You Won!'
+  elsif computer_score == 5
+    puts 'The Computer reached Five Points, You Lose!'
+  end
+
+  break if player_score == 5 || computer_score == 5
+
+  puts 'Would you like to go another round?(Y/N)'
   answer = gets.chomp.downcase
   break unless answer.start_with?('y')
 end
